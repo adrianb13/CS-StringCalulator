6f4b6e95f2a21729b64bdffcfac59744df06e6a5
Adrian Brinoes <Coding4Adrian@yahoo.com>
2019-09-06 19:00:06 -0700

    #6 - still shows error for negative numbers

--- StringCalculator/StringCalculator/Program.cs
+++ StringCalculator/StringCalculator/Program.cs
@@ -6,7 +6,7 @@ namespace StringCalculator
     {
         static void Main(string[] args)
         {
-            string input = "//;\n2;5,2,as;1001";
+            string input = "//;\n2;5,2,-100";
 
             char dl = input[2];
             char[] separator = { ',', '\n', dl };
